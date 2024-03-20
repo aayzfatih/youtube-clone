e8edc6acc9a42c8874574821adf8682806ed62a3
aayzfatih <fatihayaz076@gmail.com>
2024-03-20 14:32:06 +0300

    added videos section

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,6 +1,15 @@
 import { Box, Stack, Typography } from "@mui/material";
 import { Sidebar, Videos } from "./";
+import { useEffect, useState } from "react";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState("New");
+  const [videos, setVideos] = useState([]);
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then((data) => {
+      setVideos(data.items);
+    });
+  }, [selectedCategory]);
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
       <Box
@@ -10,7 +19,10 @@ const Feed = () => {
           px: { sx: 0, md: 2 },
         }}
       >
-        <Sidebar />
+        <Sidebar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
         <Typography
           className="copyrght"
           variant="body2"
@@ -27,9 +39,10 @@ const Feed = () => {
           mb={2}
           sx={{ color: "white" }}
         >
-          New<span style={{ color: "red" }}>videos</span>
+          {selectedCategory}
+          <span style={{ color: "red" }}> videos</span>
         </Typography>
-        <Videos videos={[]} />
+        <Videos videos={videos} />
       </Box>
     </Stack>
   );
